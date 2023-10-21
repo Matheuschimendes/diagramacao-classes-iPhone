0c18240a17219c31d7ff10f273b0c74f40933852
Curso de JS <matheuschiimenndes@gmail.com>
2023-10-21 16:52:11 -0300

    Incluindo projeto

--- src/AparelhoTelefonico.java
+++ src/AparelhoTelefonico.java
@@ -19,4 +19,46 @@ public class AparelhoTelefonico implements ReprodutorMusical, NavegadorInternet,
         throw new UnsupportedOperationException("Unimplemented method 'endCall'");
     }
 
+    @Override
+    public void openURL(String url) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'openURL'");
+    }
+
+    @Override
+    public void browseWeb() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'browseWeb'");
+    }
+
+    @Override
+    public void closeBrowser() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'closeBrowser'");
+    }
+
+    @Override
+    public void playMusic() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'playMusic'");
+    }
+
+    @Override
+    public void stopMusic() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'stopMusic'");
+    }
+
+    @Override
+    public void nextTrack() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'nextTrack'");
+    }
+
+    @Override
+    public void previousTrack() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'previousTrack'");
+    }
+
 }
